fdacfcdc295d22904b27e3f0843ddd7b22b86128
Mike Duffey <mpduffey@gmail.com>
2016-09-14 04:25:42 +0000

    MadeButtonVisible

--- intro-section.ts
+++ intro-section.ts
@@ -9,8 +9,8 @@ import {Component} from '@angular/core';
 				<div class="intro-full-height container" style="min-height: 500px; height: 100%;">
 					<div class="intro-content">
 						<div class="intro-content-inner">
-<!--							<h1 class="intro-title mb-10">Wisconsin Women for Trump</h1>
-							<a class="btn btn-lg btn-white scroll-down" href="#Statement1">I Support Donald Trump!</a> -->
+							<h1 class="intro-title mb-10">Wisconsin Women for Trump</h1>
+							<a class="btn btn-lg btn-white scroll-down" href="https://docs.google.com/forms/d/e/1FAIpQLSfjiqYbdEAR7KZbRGCiiJCGUIsr-QR2tZnSKVl2o5qs0vtaTQ/viewform" target="_blank">I Support Donald Trump! Sign Me Up!</a>
 						</div>
 					</div>
 				</div>
@@ -19,7 +19,11 @@ import {Component} from '@angular/core';
 	`,
 	styles:					[`
 		a {color: #333; cursor: pointer; text-decoration: none;}
+		.overlay-dark20 {position: relative; z-index: 1;}
+		.overlay-dark20:before {left: 0; top: 0; position: absolute; width: 100%; height: 100%; content: ""; z-index: -1;}
+		.overlay-dark20:before {background-color: rgba(34,34,34,0.2);}
 		#intro {position: relative;}
+		.intro-fullscreen {min-height: 500px; height: 100%;}
 		.intro-media {width:100%; position:absolute; display:block; top:0; left:0; height:100%;}
 		.intro-caption-waper {height:100%; position:absolute; width:100%; z-index:2;}
 		.intro-content {display:table; height: 100%; text-align:center; width: 100%;}
@@ -27,11 +31,9 @@ import {Component} from '@angular/core';
 		.intro-img-circle {margin-left:auto; margin-right:auto; width:120px; height:120px; border:5px solid rgba(0,0,0, 0.2); border-radius:50%; -moz-border-radius:50%; -webkit-border-radius:50%; margin-bottom:30px;}
 		.intro-img-circle img {outline:none; border:none; width:100%; height:100%; border-radius:50%; -moz-border-radius:50%; -webkit-border-radius:50%;}
 		@media (max-width: 767px) {
-			.intro-title {letter-spacing:0; font-size:24px;}
+			.intro-title {letter-spacing:0; font-size:18px;}
+			.btn{font-size: 10px;}
 		}
-		.overlay-dark20 {position: relative; z-index: 1;}
-		.overlay-dark20:before {left: 0; top: 0; position: absolute; width: 100%; height: 100%; content: ""; z-index: -1;}
-		.overlay-dark20:before {background-color: rgba(34,34,34,0.2);}
 	`]
 })
 
